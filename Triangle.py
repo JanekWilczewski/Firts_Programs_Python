2d80d92631219f30c80626bae1f31b54db932656
Janek <58487625+JanekWilczewski@users.noreply.github.com>
2020-03-15 00:50:31 +0100

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,5 +1,6 @@
 import math
 
+#Asks for numbers
 a = int(input("Provide a: "))
 b = int(input("Provide b: "))
 c = int(input("Provide c: "))
@@ -10,18 +11,20 @@ list.append (a)
 list.append (b)
 list.append (c)
 
-
+#Checks if the triangle exists
 if a < b + c and b < a + c and c < b + a:
     print("The triangle exists")
     print("Circuit is:", (a + b + c))
 else:
     print("The triangle does not exist")
 
+#Calculates with pythagorean wether the triangle is rectangular
 if a ** 2 + b ** 2 == c ** 2:
     print("Triangle rectangular")
 else:
     print("Triangle not rectangular")
 
+#Calculates the area of a triangle
 if a < b + c and b < a + c and c < b + a:
    F = (a + b + c) / 0.5
 
