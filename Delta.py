8aa693fc97f135a88991c785990a19950121b8cb
Janek <58487625+JanekWilczewski@users.noreply.github.com>
2020-03-15 00:28:03 +0100

    Update Delta.py

--- Delta.py
+++ Delta.py
@@ -1,9 +1,13 @@
+#Hello, I created a script that calculates the delta with its zero places
+
 from math import *
 
+#Gets numbers from the user
 a = int(input("Provide a: "))
 b = int(input("Provide b: "))
 c = int(input("Provide c: "))
 
+#Calculates a delta from the formula
 delta = b**2 - 4 * (a * c)
 
 if delta < 0:
@@ -11,6 +15,7 @@ if delta < 0:
 
 else:
     if delta > 0:
+#Calculates x1 and x2     
         x1 = -b - sqrt(delta) / (2 * a)
         x2 = -b + sqrt(delta) / (2 * a)
 
