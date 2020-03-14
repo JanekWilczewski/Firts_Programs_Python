28227a59a3b15f8176ec1d6fbc972400fb9c5619
Janek <58487625+JanekWilczewski@users.noreply.github.com>
2020-03-15 00:31:34 +0100

    Update Leap year.py

--- Leap year.py
+++ Leap year.py
@@ -1,8 +1,9 @@
+#Hello!, I created a script that checks if a given year is leap or not
+
+#Asks for a year
 year = int(input("Provide year: "))
 
-
-divisible4 = year / 4
-
+#Calculates when it is leap
 if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
     print("Leap year")
 
