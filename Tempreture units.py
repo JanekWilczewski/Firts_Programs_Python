3d7312dd191a0a2a376eaf1604208bad40da0636
Janek <58487625+JanekWilczewski@users.noreply.github.com>
2020-03-15 00:36:45 +0100

    Update Tempreture units.py

--- Tempreture units.py
+++ Tempreture units.py
@@ -1,12 +1,16 @@
+#Hi! I have created a script that converts temperature units
+
+#Asks for unit
 unit = input("Select your units; Celsius - c, Fahrenheit - f, Calvin - k: ")
 
+#Calculates units relative to Celsius
 if unit == "c":
     c = int(input("Enter temperature in Celsius: "))
     f = (c * 1.8) + 32
     k = c + 273.15
     print(c,"C = ",f,"F")
     print(c,"C =",k,"K" )
-
+#Calculates units relative to Fahrenheit
 elif unit == "f":
     f = int(input("Enter temperature in Fahrenheit: "))
     c = (f - 32) / 1.8
@@ -14,6 +18,7 @@ elif unit == "f":
     print(f, "F = ",c, "C")
     print(f, "F =",k, "K")
 
+#Calculates units relative to Kelvin   
 elif unit == "k":
     k = int(input("Enter temperature in calvin (Kelvin): "))
     c = k - 273.15
